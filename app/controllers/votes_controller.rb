fcbe218f74c0ecac0b0e34d03a588bedc70d8c1a
ankur kothari <ankothari@gmail.com>
2013-12-15 07:10:09 +0530

    removed duplication

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,18 +3,13 @@ class VotesController < ApplicationController
   
   def create
     current_user.likes question
-    Pusher['test_channel'].trigger('my_event', {
-         message: 'hello world'
-       })
-   
+    alert_client
     redirect_to root_path, notice: "Now following user."
   end
   
   def destroy
     current_user.dislikes question
-    Pusher['test_channel'].trigger('my_event', {
-         message: 'hello world'
-       })
+    alert_client
     redirect_to root_path, notice: "No longer following user."
   end
   
@@ -22,5 +17,11 @@ class VotesController < ApplicationController
   
   def question
      @_question ||= Question.find(params[:question_id])
+  end
+  
+  def alert_client
+    Pusher['test_channel'].trigger('my_event', {
+         message: 'hello world'
+       })
   end
 end
