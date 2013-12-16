824fd7c158e9bdede9a636ff4f62b8b9ddf66001
ankur kothari <ankothari@gmail.com>
2013-12-16 14:26:08 +0530

    view_homepage_spec passng

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_filter :authenticate_admin!
   def new
     @event = Event.new
   end
--- spec/integration/view_homepage_spec.rb
+++ spec/integration/view_homepage_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
 feature "View the homepage" do
-  scenario "users sees relecant information" do
+  scenario "users sees links at the top" do
     visit root_path
-    expect(page).to have_title "Ask your question!" 
-    expect(page).to have_css "[data-role='nav-links']"
-    # expect(page).to have_css('a img', src: "/assets/twitter.png")
-  #   expect(page).to have_css('a img', src: "/assets/facebook.png")
-    expect(page).to have_content "Signed in as Guest"
+    page.should have_title "Ask your question!" 
+    page.should have_css "[data-role='nav-links']"
+    page.should have_css('a img[src="/assets/twitter.png"]')
+    page.should have_css('a img[src="/assets/facebook.png"]')
+    page.should have_content "Signed in as Guest"
+    page.should have_link "Admin Login"
+    page.should have_link "Sign up to create an event."
+    page.should have_link "Create an event"
+    page.should have_link "Join an event"
   end
 end
 
