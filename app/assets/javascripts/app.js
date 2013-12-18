0f4eb25bacd15270cb19c9fbb2dee69e0ed3bae4
ankur kothari <ankothari@gmail.com>
2013-12-18 21:14:48 +0530

    made sure angular is wth the production env

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,42 @@
-var app, re;
+app = angular.module("App", ["ngAnimate"])
 
-app = angular.module('App', ['ngAnimate']);
-
-app.factory('fromoutside', function($window, $q, $rootScope) {
-  $window.pubnub = PUBNUB.init({
+app.factory("fromoutside", [
+  "$window", function($window) {
+     $window.pubnub = PUBNUB.init({
        publish_key: 'pub-75a3aad0-d9c8-4763-8838-9aae1226ed0c',
        subscribe_key: 'sub-fdd15440-8939-11e1-aa2b-41374c390533'
    });
-});
+  }
+]);
+// 
+// 
+// app.factory("fromoutside", [
+//   "$window", function($window) {
+//     return $resource("/groups/:verb", {
+//       verb: ""
+//     }, {
+//       update: {
+//         method: "PUT",
+//         isArray: true,
+//         params: {
+//           name: "",
+//           verb: "find_by_name"
+//         }
+//       },
+//       createGroup: {
+//         method: "POST",
+//         params: {
+//           name: "",
+//           code: ""
+//         }
+//       }
+//     });
+//   }
+// ]);
+
+// app.factory "fromoutside", ["$window", ($window)
+//   $window.pubnub = PUBNUB.init(
+//     publish_key: "pub-75a3aad0-d9c8-4763-8838-9aae1226ed0c"
+//     subscribe_key: "sub-fdd15440-8939-11e1-aa2b-41374c390533"
+//   )
+// ]
